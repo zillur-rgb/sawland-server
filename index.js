79f920aa005b300206f4d09a65a8794e427a704f
zillur-rgb <zillurdeu@gmail.com>
2022-05-23 19:26:55 +0200

    deleting order

--- index.js
+++ index.js
@@ -41,6 +41,14 @@ async function run() {
       res.send(allOrders);
     });
 
+    // Deleting an order
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // PUT request in the tools to reduce stock and increase sold
     app.put("/tools/:id", async (req, res) => {
       const id = req.params.id;
