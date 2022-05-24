c387e5434f98d3b7dc8d10b5aae82e17c05a5fe2
zillur-rgb <zillurdeu@gmail.com>
2022-05-24 11:30:13 +0200

    Find out if an user admin or normal user

--- index.js
+++ index.js
@@ -59,6 +59,14 @@ async function run() {
       res.send(result);
     });
 
+    // Find out if admin
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ isAdmin: isAdmin });
+    });
+
     // PUT request to make admin
     app.put("/users/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
