197f109e76ccc74ded99ad800f4396a84c702e29
zillur-rgb <zillurdeu@gmail.com>
2022-05-26 19:04:03 +0200

    Heroku ready

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
@@ -54,18 +52,6 @@ async function run() {
         }
       );
     };
-
-    // app.post("/create-payment-intent", async (req, res) => {
-    //   const order = req.body;
-    //   const price = order.price;
-    //   const amount = price * 100;
-    //   const paymentIntent = await stripe.paymentIntents.create({
-    //     amount: amount,
-    //     currency: "eur",
-    //     payment_method_types: ["card"],
-    //   });
-    //   res.send({ clientSecret: paymentIntent.client_secret });
-    // });
 
     // Post a blog
     app.post("/blogs", verifyJWT, async (req, res) => {
