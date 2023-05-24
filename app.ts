e9c0e4d831249a1d97c1ff60c6f1f9c21c26c4ca
zillurclub <zillurdeu@gmail.com>
2023-05-24 17:26:11 +0200

    Added Blog Schema and interface

--- app.ts
+++ app.ts
@@ -1,10 +1,31 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { Schema } from "mongoose";
 
 const app: Application = express();
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
+  interface IBlog {
+    title: string;
+    desc: string;
+    img?: string;
+  }
+
+  const blogSchema = new Schema<IBlog>({
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+  });
   res.send("Hello World");
 });
 
