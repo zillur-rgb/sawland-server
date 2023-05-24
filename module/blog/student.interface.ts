58c6aefee32f1a30991378c7d0e42bb38ce9ea79
zillurclub <zillurdeu@gmail.com>
2023-05-24 19:28:17 +0200

    Added interface and schema in module

---
+++ module/blog/student.interface.ts
@@ -0,0 +1,5 @@
+interface IBlog {
+  title: string;
+  desc: string;
+  img?: string;
+}
---
+++ module/blog/student.model.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+import { IBlog } from "./student.interface";
+
+export const blogSchema = new Schema<IBlog>({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+  },
+});
+
+// Creating the model
+const Blog = model<IBlog>("Blog", blogSchema);
+export default Blog;
